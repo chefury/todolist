b4abfb530beba0319c99f202285790552db44293
chefuny <nhclei@gmail.com>
2024-06-10 11:12:37 +0800

    feat: add Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Header from './components/Header'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+
+
+      <Header />
+
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () =>{
+    return (
+        <header>
+            <h1>ToDo App</h1>
+        </header>
+    );
+}
+export default Header;
